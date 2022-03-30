d0a91e563c855476d671c2f9be0cb81981d4c2e8
pyatang <longyangwillgofuther@gmail>
2022-03-30 17:00:17 +0800

    initial project

---
+++ src/main/java/org/lytang/littletool/LittleTool.java
@@ -0,0 +1,13 @@
+package org.lytang.littletool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleTool {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittleTool.class, args);
+    }
+
+}
